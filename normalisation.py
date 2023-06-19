fbc7d79a7323d03b7082c5333a57e8b83c5b2e91
Akshat Kumar <93365394+Ak3199@users.noreply.github.com>
2023-06-19 09:39:04 +0530

    Add files via upload

---
+++ normalisation.py
@@ -0,0 +1,9 @@
+from sklearn import preprocessing
+
+
+def normalise(file_dataframe, cols):
+    for col in cols:
+        preprocessing.normalize(file_dataframe[col],
+                                axis=1, norm='l2', copy=False)
+
+    return file_dataframe
